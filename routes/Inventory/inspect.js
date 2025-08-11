f6c04c511b1776aea1cf86069ff5145d7c089008
Linh3694 <hailinh.n23@gmail.com>
2025-08-11 17:18:12 +0700

    update

--- routes/Inventory/inspect.js
+++ routes/Inventory/inspect.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const inspectController = require('../../controllers/Inventory/inspectController');
-const validateToken = require('../../middleware/validateToken');
+const { authenticateServiceOrUser, optionalAuth } = require('../../middleware/validateToken');
 const router = express.Router();
 const uploadReport = require('../../middleware/uploadReport');
 
-router.get('/', inspectController.getAllInspections);
-router.get('/:id', inspectController.getInspectionById);
+// Public/optional auth for GET endpoints
+router.get('/', optionalAuth, inspectController.getAllInspections);
+router.get('/latest/:deviceId', optionalAuth, inspectController.getLatestInspectionByDeviceId);
+router.get('/downloadReport/:inspectId', optionalAuth, inspectController.downloadReport);
+router.get('/:id', optionalAuth, inspectController.getInspectionById);
+
+// Protected writes
+router.use(authenticateServiceOrUser);
+router.post('/', inspectController.createInspection);
 router.put('/:id', inspectController.updateInspection);
-router.post('/', validateToken, inspectController.createInspection);
 router.delete('/:id', inspectController.deleteInspection);
-router.get('/latest/:deviceId', inspectController.getLatestInspectionByDeviceId);
 router.post('/uploadReport', uploadReport.single('file'), inspectController.uploadReport);
-router.get('/downloadReport/:inspectId', inspectController.downloadReport);
 
 module.exports = router;
 
